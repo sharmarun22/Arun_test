95003b660e08f46c9e2869230f083d201a0e8f4f
sharmarun22 <sharmarun22@gmail.com>
2020-06-15 23:17:35 +0530

    Scenario B implemented

--- MainProject/Program.cs
+++ MainProject/Program.cs
@@ -13,41 +13,62 @@ namespace MainProject
         int item_B_Value = 30;
         int item_C_Value = 20;
         int item_D_Value = 15;
+        int promotionValueForA_eachOfThree = 130;
+        int promotionValueForB_eachOfTwo = 45;
+        int promotionValueForCAndD_Only = 15;
         public static void Main(string[] args)
         {
             
-
+           
         }
 
-        //private int AfterPromotionValue(int item_A_Quantity, int item_B_Quantity,int item_C_Quantity,int item_D_Quantity)
-        //{
-        //    int promotionForA = 3;
-        //    int promotionForB = 2;
-        //    int promotionForCAndD = 1;
-            
-        //    if (item_A_Quantity !=0)
-        //    {
-        //        int calculativeValue = item_A_Quantity / promotionForA;
-        //    }
-        //    if (item_B_Quantity != 0)
-        //    {
-        //    }
-        //    if (item_C_Quantity != 0 && item_D_Quantity!=0)
-        //    { 
-        //    }
-        //    return 0;
-        //}
+        private int AfterPromotionValue(int item_A_Quantity, int item_B_Quantity, int item_C_Quantity, int item_D_Quantity)
+        {
+            int promotionForA = 3;
+            int promotionForB = 2;
+            int promotionForCAndD = 1;
+            int total = 0;
+            int prmotionValue = 0;
+            int excludedFromPromotionValue = 0;
+
+            if (item_A_Quantity != 0)
+            {
+                prmotionValue = item_A_Quantity / promotionForA;
+                excludedFromPromotionValue = item_A_Quantity - (prmotionValue * promotionForA);
+                total = (prmotionValue * promotionValueForA_eachOfThree) + (excludedFromPromotionValue * item_A_Value);
+            }
+            if (item_B_Quantity != 0)
+            {
+                prmotionValue = item_B_Quantity / promotionForB;
+                excludedFromPromotionValue = item_B_Quantity - (prmotionValue * promotionForB);
+                total += (prmotionValue * promotionValueForB_eachOfTwo) + (excludedFromPromotionValue * item_B_Value);
+            }
+            if (item_C_Quantity != 0 && item_D_Quantity != 0)
+            {
+
+            }
+            else
+            {
+                if(item_C_Quantity!=0)
+                {
+                    total += item_C_Quantity * item_C_Value;
+                }
+            }
+            return total;
+        }
+
+       
         public int CheckoutScenarioA(int item_A_Quantity, int item_B_Quantity, int item_C_Quantity)
         {
             return (item_A_Quantity * item_A_Value) + (item_B_Quantity * item_B_Value) + (item_C_Quantity * item_C_Value);
         }
 
-        //public int CheckoutScenarioB(int item_A_Quantity, int item_B_Quantity, int item_C_Quantity)
-        //{
-        //    return (item_A_Quantity * item_A_Value) + (item_B_Quantity * item_B_Value) + (item_C_Quantity * item_C_Value);
-        //}
+        public int CheckoutScenarioB(int item_A_Quantity, int item_B_Quantity, int item_C_Quantity, int item_D_Quantity)
+        {
+            return AfterPromotionValue(item_A_Quantity, item_B_Quantity, item_C_Quantity, item_D_Quantity);
+        }
 
     }
-
+   
 
 }
--- UnitTestProject/UnitTest.cs
+++ UnitTestProject/UnitTest.cs
@@ -20,11 +20,14 @@ namespace UnitTestProject
         [TestMethod]
         public void CheckScenarioB()
         {
-            int item_A_Quantity = 1;
-            int item_B_Quantity = 1;
+            int item_A_Quantity = 5;
+            int item_B_Quantity = 5;
             int item_C_Quantity = 1;
+            int item_D_Quantity = 0;
+
             var ObjProg = new MainProject.Program();
-            Assert.AreEqual(ObjProg.CheckoutScenarioB(item_A_Quantity, item_B_Quantity, item_C_Quantity), 100);
+          
+            Assert.AreEqual(ObjProg.CheckoutScenarioB(item_A_Quantity, item_B_Quantity, item_C_Quantity,item_D_Quantity), 370);
         }
 
 
