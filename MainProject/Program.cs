e70d56793e70418a8d692541b560dfbe9d8b69e7
sharmarun22 <sharmarun22@gmail.com>
2020-06-15 22:25:13 +0530

    Created Test Project and Console Project .
    Scenarios A , Failed Test method Added

---
+++ MainProject/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace MainProject
+{
+    public class Program
+    {
+        int item_A_Value = 50;
+        int item_B_Value = 30;
+        int item_C_Value = 20;
+        int item_D_Value = 15;
+        public static void Main(string[] args)
+        {
+            
+
+        }
+
+        public void CheckoutScenario1(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+        //public int CheckoutScenario1(int iteam_A_Quantity, int iteam_B_Quantity, int iteam_C_Quantity)
+        //{
+        //    return (iteam_A_Quantity * item_A_Value) + (iteam_B_Quantity * item_B_Value) + (iteam_C_Quantity * item_C_Value);
+        //}
+
+    }
+
+
+}
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void CheckScenarioA()
+        {
+            int iteam_A_Quantity = 1;
+            int iteam_B_Quantity = 1;
+            int iteam_C_Quantity = 1;
+            var ObjProg = new MainProject.Program();
+            ObjProg.CheckoutScenario1(2, 1, 1);
+           //int totalValue = CheckoutScenario1(iteam_A_Quantity, iteam_B_Quantity, iteam_C_Quantity);
+
+        }
+
+
+    }
+}
